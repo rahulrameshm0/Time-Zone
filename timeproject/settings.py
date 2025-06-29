b1b74a033ccb35f37c764359e9117dbd9f936438
rahulrameshm0 <iamrahulm23@gmail.com>
2025-06-29 23:49:11 +0530

    Deploy your Django project with a PostgreSQL database to Render.com.

--- timeproject/settings.py
+++ timeproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,14 +79,11 @@ WSGI_APPLICATION = 'timeproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'watch_cart',
-        'USER':'postgres',
-        'PASSWORD':'724850',
-        'HOST': 'localhost'
-
-    }
+      'default': dj_database_url.config(
+        default='postgres://postgres:724850@localhost:5432/watch_cart',
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 # Password validation
